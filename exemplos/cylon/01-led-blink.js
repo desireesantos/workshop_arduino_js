4a20bd6ea4254523855ca0e18b048343d1803348
Desiree Santos <desisant@gmail.com>
2016-07-18 01:04:02 -0300

    Update robot name and comments

--- exemplos/cylon/01-led-blink.js
+++ exemplos/cylon/01-led-blink.js
@@ -1,13 +1,12 @@
 const Cylon = require('cylon')
-const serialPort = '/dev/cu.usbmodem1421' //Adicione a porta serial aqui
+const serialPort = '/dev/cu.usbmodem1421' //Adicione a sua porta serial aqui
 
 Cylon
-	.robot({name: 'desi'})
+	.robot({name: 'desiRobot'})
 	.connection( 'arduino', {adaptor: 'firmata', port: serialPort})
 	.device( 'led', {driver: 'led', pin: 13})
 	.on('ready', function(my) {
 		my.led.toggle();
 	});
-
 
 Cylon.start();

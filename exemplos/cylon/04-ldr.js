a1562b8164f07846d0ee7b810bce9f0550050e82
Desiree Santos <desisant@gmail.com>
2016-07-20 00:28:59 -0300

    Adding ldr example

---
+++ exemplos/cylon/04-ldr.js
@@ -0,0 +1,28 @@
+const Cylon = require('cylon')
+const serialPort = ''
+
+
+Cylon
+	.robot({name: 'desiRobot'})
+	.connection( 'arduino', {adaptor: 'firmata', port: serialPort})
+	.device( 'sensor', {driver: 'analog-sensor', pin: 0, lowerLimit: 100, upperLimit: 900})
+	.on('ready', function(my) {
+		
+		every((1).second(), function() {
+	      analogValue = my.sensor.analogRead();
+	      console.log('Analog value => ', analogValue);
+	    });
+
+	    my.sensor.on('lowerLimit', function(val) {
+      		console.log("Lower limit reached!");
+      		console.log('Analog value => ', val);
+    	});
+
+    	my.sensor.on('upperLimit', function(val) {
+      		console.log("Upper limit reached!");
+      		console.log('Analog value => ', val);
+    	});
+
+	});
+
+Cylon.start();
